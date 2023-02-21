f6ffbf7d84c8dfada5137eac276a353dbd947ff9
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-02-22 07:28:03 +0900

    add lib/posts.js,post_list-function

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,15 @@ import Link from 'next/link'
 import { useSelector, useDispatch } from 'react-redux'
 import { logout } from '../actions/auth'
 
+import {
+  HomeIcon,
+  LogoutIcon,
+  LoginIcon,
+  PlusCircleIcon,
+  UserAddIcon,
+  UserIcon,
+} from '@heroicons/react/outline'
+
 const Navigation = () => {
   const dispatch = useDispatch()
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
@@ -13,38 +22,57 @@ const Navigation = () => {
   }
 
   return (
-    <>
-      <div className="bg-gray-900">
-        <div className="max-w-7xl mx-auto px-8 py-6">
-          <div className="flex items-center justify-between">
-            <div>
-              <Link href="/" legacyBehavior>
-                <a className="text-white hover:text-gray-50 font-extrabold text-lg">
-                  フルスタックチャンネル
-                </a>
-              </Link>
-            </div>
-            <div className="">
-              {isAuthenticated ? (
-                <div onClick={logoutHandler} className="cursor-pointer button-nav">
-                  ログアウト
+  <div className="sticky top-0 bg-white z-10">
+      <div className="border-b py-3">
+        <div className="max-w-5xl mx-auto flex justify-between px-4">
+          <div className="text-lg font-extrabold">
+            <Link href="/" legacyBehavior>
+              <a>FullStackChannel</a>
+            </Link>
+          </div>
+          <div className="flex space-x-4">
+            <Link href="/" legacyBehavior>
+              <a>
+                <HomeIcon className="h-7 w-7" />
+              </a>
+            </Link>
+
+            {isAuthenticated ? (
+              <div className="flex space-x-4">
+                <Link href="/post/new" legacyBehavior>
+                  <a>
+                    <PlusCircleIcon className="h-7 w-7" />
+                  </a>
+                </Link>
+                <Link href="/profile" legacyBehavior>
+                  <a>
+                    <UserIcon className="h-7 w-7" />
+                  </a>
+                </Link>
+                <div onClick={logoutHandler} className="cursor-pointer">
+                  <LogoutIcon className="h-7 w-7" />
                 </div>
-              ) : (
-                <div>
-                  <Link href="/login" legacyBehavior>
-                    <a className="button-nav mr-4">ログイン</a>
-                  </Link>
-                  <Link href="/register" legacyBehavior>
-                    <a className="button-nav">アカウント登録</a>
-                  </Link>
-                </div>
-              )}
-            </div>
+              </div>
+            ) : (
+              <div className="flex space-x-4">
+                <Link href="/login" legacyBehavior>
+                  <a>
+                    <LoginIcon className="h-7 w-7" />
+                  </a>
+                </Link>
+                <Link href="/register" legacyBehavior>
+                  <a>
+                    <UserAddIcon className="h-7 w-7" />
+                  </a>
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
+
 
 export default Navigation
