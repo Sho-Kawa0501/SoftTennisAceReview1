567eff622e180214cd852c7058414619dc237754
Sho-Kawa0501 <akirasho0501@gmail.com>
2023-12-21 21:39:38 +0900

    edit getreviewlist2

--- src/lib/review/getAllReview.ts
+++ src/lib/review/getAllReview.ts
@@ -20,6 +20,7 @@ const getAllReview = async (
 ): Promise<GetReview> => {
   try {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_PATH}/api/review_list/${itemId}`, {
+      withCredentials: true, 
     });
 
     return {
